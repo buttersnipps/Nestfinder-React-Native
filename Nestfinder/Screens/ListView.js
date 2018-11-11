16ce84d16313897ead7681824554845a4e251c5b
Soutrik Barua <sbarua91@gmail.com>
2018-11-10 23:32:48 -0500

    Grid view implemented with new navigational routing

--- Nestfinder/Screens/ListView.js
+++ Nestfinder/Screens/ListView.js
@@ -2,15 +2,53 @@ import React,{Component} from "react";
 import{
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    Image,
+    SafeAreaView
 } from "react-native";
+import GridView from 'react-native-super-grid';
+
 
 class ListView extends Component{
     render(){
+        const items = [
+            { name: '66 Haynes',imageUri:require('../assets/Images/Houses/house1.jpeg'),price:"$2000"}, 
+            { name: '36 Evelyn Wiggins',imageUri:require('../assets/Images/Houses/house2.jpeg'),price:"$2500"},
+            { name: '70 Pond Road',imageUri:require('../assets/Images/Houses/house3.jpeg'),price:"$2700"}, 
+            { name: '80 Lulu Street',imageUri:require('../assets/Images/Houses/house4.jpeg'),price:"$3000"},
+          ];
         return(
-            <View style ={styles.container}>
-                <Text>List View</Text>
-            </View>
+           <SafeAreaView style={{flex:1}}>
+           <Text style={{fontSize: 24 , fontWeight:'700',
+                            paddingHorizontal : 20 ,backgroundColor:'white' }}>
+                             Available Homes near you..
+            </Text>
+            <GridView
+            itemDimension={130}
+            items={items}
+            style={styles.gridView}
+            renderItem={item => (
+                <View style = {{height: 130,width :130,
+                    marginLeft : 20, borderWidth: 0.5,
+                    borderColor: '#dddddd'}} >
+                    <View style={{flex: 2}}>
+                        <Image source={item.imageUri} 
+                        style={{flex:1, width: null,
+                        height: null, resizeMode: 'cover'}} 
+                        />
+                    </View>
+                    <View style={{flex: 1 , paddingLeft: 10,paddingTop: 10}}>
+                            <Text style={{fontWeight:"bold"}}> {item.name} </Text>    
+                    </View>
+                    <View style={{flex: 1 , paddingLeft: 10}}>
+                            <Text> {item.price} </Text>    
+                    </View>
+                </View>
+            )}
+            />
+           </SafeAreaView>
+          
+          
         );
     }
 }
@@ -18,9 +56,10 @@ export default ListView;
 
 const styles = StyleSheet.create({
 
-container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-},
+    gridView: {
+        paddingTop: 25,
+        flex: 1,
+        backgroundColor: 'white'
+      },
+     
 });
