bc07162414196690e806f8db8c5b424fb83576d8
Soutrik Barua <sbarua91@gmail.com>
2018-11-12 14:44:07 -0500

    successfully gettting the objects from firebase

--- Nestfinder/Screens/ListView.js
+++ Nestfinder/Screens/ListView.js
@@ -4,26 +4,33 @@ import{
     Text,
     StyleSheet,
     Image,
-    SafeAreaView
+    SafeAreaView,
+    FlatList
 } from "react-native";
 import GridView from 'react-native-super-grid';
 import * as firebase from 'firebase';
 
-class ListView extends Component{
-    
-    
-    componentWillMount(){
-  
-        var config = {
-            apiKey: "AIzaSyAtwTUv9Dr608Fv4x7QTcWEfbos-HKyuk8",
-            authDomain: "nestfinder-react-natice.firebaseapp.com",
-            databaseURL: "https://nestfinder-react-natice.firebaseio.com",
-            projectId: "nestfinder-react-natice",
-            storageBucket: "nestfinder-react-natice.appspot.com",
-            messagingSenderId: "502710052203"
-        };
-        firebase.initializeApp(config);
+var config = {
+    apiKey: "AIzaSyAtwTUv9Dr608Fv4x7QTcWEfbos-HKyuk8",
+    authDomain: "nestfinder-react-natice.firebaseapp.com",
+    databaseURL: "https://nestfinder-react-natice.firebaseio.com",
+    projectId: "nestfinder-react-natice",
+    storageBucket: "nestfinder-react-natice.appspot.com",
+    messagingSenderId: "502710052203"
+};
+firebase.initializeApp(config);
+const rootRef = firebase.database().ref();
+const houseRef = rootRef.child('houses');
 
+class ListView extends Component{
+    constructor(props){
+        super(props);
+        this.state = ({
+            houses : [],
+            loading : false,
+        });
+    }
+    componentWillMount(){
         function writeUserData(houseId, address, price) {
             firebase.database().ref('houses/' + houseId).set({
               address: address,
@@ -31,23 +38,28 @@ class ListView extends Component{
             });
           }
        //   writeUserData(2,'36 Evelyn Wiggins','2500');
-        var fetchHouses = firebase.database().ref('houses/');
-      this.list =  fetchHouses.once('value').then(snapshot => { 
+        //var fetchHouses = firebase.database().ref('houses/');
+        houseRef.once('value').then(snapshot => { 
+                const houses = [];
             snapshot.forEach(function(child){
-                this.add = child.val();    
+                var obj = child.val();
+                var add = obj.address;
+                houses.push(add);  
             });
-        })  
-       // this.setState({list : this.state.list})  
+
+            this.setState({ houses : houses});
+        });   
     }
        
     render(){
-       const items = [
+      /* const items = [
             { name: '66 Haynes',imageUri:require('../assets/Images/Houses/house1.jpeg'),price:"$2000"}, 
             { name: '36 Evelyn Wiggins',imageUri:require('../assets/Images/Houses/house2.jpeg'),price:"$2500"},
             { name: '70 Pond Road',imageUri:require('../assets/Images/Houses/house3.jpeg'),price:"$2700"}, 
             { name: '80 Lulu Street',imageUri:require('../assets/Images/Houses/house4.jpeg'),price:"$3000"},
-          ]
-         // var a = this.add;
+          ]*/
+          const items = this.state.houses;
+       
         return(
            <SafeAreaView style={{flex:1}}>
            <Text style={{fontSize: 24 , fontWeight:'700',
@@ -61,23 +73,14 @@ class ListView extends Component{
             renderItem={(item) => (
                 <View style = {{height: 130,width :130,
                     marginLeft : 20, borderWidth: 0.5,
-                    borderColor: '#dddddd'}} >
-                    <View style={{flex: 2}}>
-                        <Image source={item.imageUri} 
-                        style={{flex:1, width: null,
-                        height: null, resizeMode: 'cover'}} 
-                        />
-                    </View> 
+                    borderColor: '#dddddd'}} > 
                     <View style={{flex: 1 , paddingLeft: 10,paddingTop: 10}}>
-                            <Text style={{fontWeight:"bold"}}> {item.name} </Text>    
-                    </View>
-                    <View style={{flex: 1 , paddingLeft: 10}}>
-                            <Text> {item.price} </Text>    
+                            <Text style={{fontWeight:"bold"}}> {item} </Text>    
                     </View>
                 </View>
             )}
             />
-           </SafeAreaView>
+          </SafeAreaView>
           
           
         );
